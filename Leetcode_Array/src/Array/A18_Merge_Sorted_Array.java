f384a6bc0b74e6c34a78d22cdaf2c3ae67cdc9a3
北京-沈煜 <shenyu1@jd.com>
2018-10-31 15:31:36 +0800

    update111

--- Leetcode_Array/src/Array/A18_Merge_Sorted_Array.java
+++ Leetcode_Array/src/Array/A18_Merge_Sorted_Array.java
@@ -1,8 +1,47 @@
 package Array;
 
+import java.util.Arrays;
+
 /**
  * Created by happiness on 2018/10/31.
+ * Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
+
+ Note:
+
+ The number of elements initialized in nums1 and nums2 are m and n respectively.
+ You may assume that nums1 has enough space (size that is greater or equal to m + n) to hold additional elements from nums2.
+
+ Example:
+
+ Input:
+ nums1 = [1,2,3,0,0,0], m = 3
+ nums2 = [2,5,6],       n = 3
+
+ Output: [1,2,2,3,5,6]
  */
 
 public class A18_Merge_Sorted_Array {
+    public static void main(String args[]) {
+        int[] A = {1, 2, 3, 0, 0, 0};
+        int[] B = {2, 5, 6};
+        merge(A, 3, B, 3);
+    }
+
+    public static void merge(int A[], int m, int B[], int n) {
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
+        while (i >= 0 && j >= 0) {
+            if (A[i] > B[j])
+                A[k--] = A[i--];
+            else
+                A[k--] = B[j--];
+        }
+        while (j >= 0)
+            A[k--] = B[j--];
+
+        System.out.println(Arrays.toString(A));
+
+    }
+
 }
